c4a1dd606c5f8cb542b294ad3526ea82a62823cb
root <root@DESKTOP-6069T4U.localdomain>
2025-01-09 02:05:59 +0200

    Started task 2

--- src/task2.c
+++ src/task2.c
@@ -1,6 +1,26 @@
 #include "task2.h"
 
+#define NMAX 256
+
 int main(int argc, char *argv[]) {
     // TODO(student): Task 2
+    FILE* file = fopen(argv[1], "r");
+    if (file == NULL) {
+        return 0;
+    }
+
+    char* line = malloc(NMAX * sizeof(char));
+    int number = 0;
+    while (fgets(line, NMAX, file) != NULL) {
+        sscanf(line, "%d", &number);
+
+        line[strcspn(line, "\r\n")] = 0;
+
+        if (line[0] == '\0') {
+            continue;
+        }
+    }
+
+
     return 0;
 }
